0d481b8a1df9c968b58bcc86202ca8e42b0fd5e2
Avanish9795 <118121756+Avanish9795@users.noreply.github.com>
2026-03-17 11:48:32 +0530

    Add dockerpush function for Docker login and push

---
+++ vars/dockerpush.groovy
@@ -0,0 +1,6 @@
+def call(String creds, String image, String tag) {
+    withCredentials([usernamePassword(credentialsId: creds, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh "docker login -u $USER -p $PASS"
+        sh "docker push ${image}:${tag}"
+    }
+}
