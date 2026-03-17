fe564621f5dbc0a4bd596323fdc98b67a8746e23
Avanish9795 <118121756+Avanish9795@users.noreply.github.com>
2026-03-17 13:31:30 +0530

    Update Docker login and push commands

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,6 +1,8 @@
 def call(String creds, String image, String tag) {
     withCredentials([usernamePassword(credentialsId: creds, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh "docker login -u $USER -p $PASS"
-        sh "docker push avanish9795/${image}:${tag}"
+        sh """
+        echo \$PASS | /usr/bin/docker login -u \$USER --password-stdin
+        /usr/bin/docker push avanish9795/${image}:${tag}
+        """
     }
 }
