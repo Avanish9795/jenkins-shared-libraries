feedd21a5b1e6322b1ca8dc261ea3f61a2f7e45a
Avanish9795 <118121756+Avanish9795@users.noreply.github.com>
2026-03-18 12:07:54 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,8 +1,10 @@
 def call(String image, String tag) {
     sh """
-    echo "PATH=$PATH"
+    echo "PATH=\$PATH"
     which docker
     docker --version
     docker build -t avanish9795/${image}:${tag} .
     """
 }
+
+return this
