1eb369d271f862bbfc56a236a426882d03f4d91b
Avanish9795 <118121756+Avanish9795@users.noreply.github.com>
2026-03-19 14:30:17 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,8 +1,10 @@
-def call(String creds, String image, String tag) {
-    withCredentials([usernamePassword(credentialsId: creds, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh """
-        echo \$PASS | /usr/bin/docker login -u \$USER --password-stdin
-        /usr/bin/docker push avanish9795/${image}:${tag}
-        """
-    }
+withCredentials([usernamePassword(
+    credentialsId: 'dockerHubCreds',
+    usernameVariable: 'USER',
+    passwordVariable: 'PASS'
+)]) {
+    sh """
+    echo $PASS | docker login -u $USER --password-stdin
+    docker push avanish9795/${image}:${tag}
+    """
 }
