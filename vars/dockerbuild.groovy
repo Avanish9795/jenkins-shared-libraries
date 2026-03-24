66043c84beaea8b168714701df8f03ef2aa551de
Avanish9795 <118121756+Avanish9795@users.noreply.github.com>
2026-03-24 14:47:50 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,15 +1,3 @@
-def call(String dockerUser, String image, String tag) {
-    try {
-        sh """
-        echo "PATH=\$PATH"
-        which docker
-        docker --version
-
-        echo "Building Docker image: ${dockerUser}/${image}:${tag}"
-        docker build -t ${dockerUser}/${image}:${tag} .
-        """
-    } catch (Exception e) {
-        echo "Docker build failed!"
-        throw e
-    }
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
